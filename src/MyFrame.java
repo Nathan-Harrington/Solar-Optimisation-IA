5e1e0ba4858593e22337d330f75e72e2934979cb
SleepyDingo <69660768+Nathan-Harrington@users.noreply.github.com>
2023-09-25 21:02:37 +1000

    First Window
    Set up basic template which will need to be modified for greater orignality

---
+++ src/MyFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){ //Constructor code needs to be written to be personalised
+        //Creates Window
+        JFrame frame = new JFrame();
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setVisible(true);
+        this.setTitle("Solar Optimisation Application");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Sets Icon
+        ImageIcon image = new ImageIcon("images/icon.png");
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(0x212121)); // To Be Contrasted with ff4545 and 1e1c1c
+    }
+}
---
+++ src/dashboard.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class dashboard {
+    public dashboard(){
+
+    }
+}
