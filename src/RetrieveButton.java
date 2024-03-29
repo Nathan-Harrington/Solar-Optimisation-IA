75a245a203be10d47394b9579d8b028b71fc750c
SleepyDingo <69660768+Nathan-Harrington@users.noreply.github.com>
2024-03-29 19:59:57 +1100

    Update RetrieveButton.java

--- src/RetrieveButton.java
+++ src/RetrieveButton.java
@@ -7,12 +7,15 @@ public class RetrieveButton extends JButton{
         this.setPreferredSize(new Dimension(200,75));
         this.addActionListener(e -> System.out.println("schedule input received"));
         this.setText(text);
-
+        this.addActionListener(e -> displaySchedule());
         //Styling
         this.setBackground(new Color(0x8FC089));
         this.setForeground(new Color(0xFAF3DD));
         Border border = BorderFactory.createLineBorder(new Color(0x68b0ab), 3);
         this.setBorder(border);
+    }
+    public static void displaySchedule(){
+        
     }
 }
 
